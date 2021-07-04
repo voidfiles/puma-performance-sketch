346c3f0cbf9b32331c0391093d3bf81fd97c1436
Alex Kessinger <voidfiles@gmail.com>
2021-07-04 12:39:03 -0600

    Paramatarize hello benchmark

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -3,11 +3,13 @@
 # Mostly a copy of the puma hello benchmark
 # https://github.com/puma/puma/blob/master/benchmarks/wrk/hello.sh
 cd app;
+PUMA_THREADS="${PUMA_THREADS:-4}"
+WRK_CONCURRENT="${WRK_CONCURRENT:-4}"
 
-bundle exec puma -t 4 hello.ru &
+bundle exec puma -t ${PUMA_THREADS} hello.ru &
 PID1=$!
 sleep 5
-wrk -c 4 -d 30 --latency http://localhost:9292
+wrk -c ${WRK_CONCURRENT} -d 30 --latency http://localhost:9292
 
 # echo "GET http://localhost:9292" | vegeta -cpus=2 attack -rate=100/1s -max-workers=100000 -duration=30s -timeout=1s | tee results.bin | vegeta report
 # vegeta report -type=json results.bin > metrics.json
