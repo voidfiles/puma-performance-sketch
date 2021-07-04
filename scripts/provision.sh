ce5fbfa700a6e9ab1af5aedb43448d5705a68c6e
Alex Kessinger <voidfiles@gmail.com>
2021-07-03 20:14:32 -0600

    Update benchmark

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,4 +5,4 @@ apt-get install -y ruby ruby-bundler golang netdata nginx
 cp resources/.htpasswd /etc/nginx/.htpasswd
 cp resources/nginx.conf /etc/nginx/sites-enabled/default
 
-nginx restart
+systemctl restart nginx
