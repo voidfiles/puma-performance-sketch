72030568aa7e676ec9c628f24b560e131999b1cf
Alex Kessinger <voidfiles@gmail.com>
2021-07-06 09:47:49 -0600

    Int milliseconds

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PUMA_THREADS="${PUMA_THREADS:-4}"
 
-PYTHONUNBUFFERED=x /usr/share/bcc/tools/tcplife -L 9292 -s |  awk -F',' '{print "hello.tcp.connection.latency:"$10"|ms"}' | nc -w 1 -u localhost 8125 &
+PYTHONUNBUFFERED=x /usr/share/bcc/tools/tcplife -L 9292 -s |  awk -F',' '{print "hello.tcp.connection.latency:"int($10)"|ms"}' | nc -w 1 -u localhost 8125 &
 PID1=$!
 
 cd app
