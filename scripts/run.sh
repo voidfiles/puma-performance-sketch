5dfc9bfc656f504b84df1fae3fbe5dac4390688f
Alex Kessinger <voidfiles@gmail.com>
2021-07-06 08:19:55 -0600

    Fully insturmented

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,19 @@ PUMA_THREADS="${PUMA_THREADS:-4}"
 /usr/share/bcc/tools/tcplife -L 9292 | awk '{print "hello.tcp.connection.latency:"$9"|ms"}' | nc -w 1 -u localhost 8125 &
 PID1=$!
 
-(cd app && bundle exec puma -t ${PUMA_THREADS} hello.ru)
+cd app
 
-kill $PID1
+bundle exec puma -t ${PUMA_THREADS} hello.ru & 
+PID2=$!
+
+func finish {
+    kill $PID1
+    kill $PID2
+}
+
+trap finish EXIT
+
+while kill -s 0 $PID2; do
+    ss -plnt -H sport = :9292 | awk '{print "hello.accept.queue:"$2"|g"}' | nc -w 1 -u localhost 8125
+done
+
