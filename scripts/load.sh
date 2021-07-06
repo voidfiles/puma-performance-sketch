f6b30cf5e296bfd6aad7fed09daa08d7eb6a5765
Alex Kessinger <voidfiles@gmail.com>
2021-07-06 16:06:59 -0600

    load test script is dynamic

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VIRTUAL_USERS=${VIRTUAL_USERS:-2}
-DURATION=${DURATION:-30s}
+DURATION=${DURATION:-5m}
 
 K6_STATSD_ENABLE_TAGS=true k6 run -u ${VIRTUAL_USERS} -d ${DURATION} --no-connection-reuse --out statsd scripts/script.js
--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ import { check } from 'k6';
 import http from 'k6/http';
 
 export default function () {
-  let res = http.get('http://143.110.228.93:9292/');
+  let res = http.get("http://${ __ENV.APP_HOST_IP | '127.0.0.1' }:9292/");
   check(res, {
     '200': (r) => r.status === 200,
   });
