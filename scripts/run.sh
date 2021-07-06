e770c515f031c7476d949462711b3434f4795096
Alex Kessinger <voidfiles@gmail.com>
2021-07-05 20:48:35 -0600

    Update benchmark

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 PUMA_THREADS="${PUMA_THREADS:-4}"
 
+/usr/share/bcc/tools/tcplife -L 9292 | awk '{print "hello.tcp.connection.latency:"$9"|ms"}' | nc -w 1 -u localhost 8125 &
+PID1=$!
+
 (cd app && bundle exec puma -t ${PUMA_THREADS} hello.ru)
+
+kill $PID1
