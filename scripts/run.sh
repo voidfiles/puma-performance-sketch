0842a4586f409c7694b7f203aa9eac888bd49942
Alex Kessinger <voidfiles@gmail.com>
2021-07-06 09:59:29 -0600

    Semd to netcat one line at a time

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 PUMA_THREADS="${PUMA_THREADS:-4}"
 
-PYTHONUNBUFFERED=x /usr/share/bcc/tools/tcplife -L 9292 -s |  awk -F',' '{print "hello.tcp.connection.latency:"int($10)"|ms"}' | nc -w 1 -u localhost 8125 &
+function slowcat(){ while read; do sleep .05; echo "$REPLY"; done; }
+
+PYTHONUNBUFFERED=x /usr/share/bcc/tools/tcplife -L 9292 -s |  awk -F',' '{print "hello.tcp.connection.latency:"int($10)"|ms"}' | slowcat | nc -w 1 -u localhost 8125 &
 PID1=$!
 
 cd app
