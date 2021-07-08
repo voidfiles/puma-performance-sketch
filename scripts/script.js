7344185a8ea9493989154757c7e5364d1f06b032
Alex Kessinger <voidfiles@gmail.com>
2021-07-07 20:43:23 -0600

    App host thing

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ import { check } from 'k6';
 import http from 'k6/http';
 
 export default function () {
-  let res = http.get(`http://${ __ENV.APP_HOST_IP | '127.0.0.1' }:9292/`);
+  let res = http.get(`http://${ __ENV.APP_HOST_IP }:9292/`);
   check(res, {
     '200': (r) => r.status === 200,
   });
